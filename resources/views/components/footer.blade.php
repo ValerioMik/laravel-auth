7f15b93bce01d0e956a4f482a53f8a1a761b39d7
ValerioMik <valerio.avallone@hotmail.com>
2022-02-02 17:03:36 +0100

    scafolding piu inserimento di js

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <h2>
+        arrivederci e grazie
+    </h2>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main-layout')
+@section('content')
+   <h1>per andare avanti inscriviti</h1>
+@endsection
